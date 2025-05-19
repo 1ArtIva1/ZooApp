7a96e4ddf5003f86071ef7bd9322c1564d4edbe0
Davud Akhmedov <davud_a@vk.com>
2025-05-19 23:40:12 +0300

    l

--- Home/Pages/Storage.xaml.cs
+++ Home/Pages/Storage.xaml.cs
@@ -38,7 +38,7 @@ namespace Home.Pages
         //public class Product
         //{
         //    public string Name { get; set; }
-        //    public int Quantity { get; set; }
+        //    public int Quantity { get; set; } 
         //    public string Category { get; set; }
         //    public decimal Price { get; set; }
         //}
